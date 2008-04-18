57b7e5db005ad72bd94e8a6425d98be190d3a753
Antony Dovgal <tony2001@php.net>
2008-04-18 07:31:51 +0000

    prepare to 1.0.0

--- php_tdb.h
+++ php_tdb.h
@@ -24,7 +24,7 @@
 extern zend_module_entry tdb_module_entry;
 #define phpext_tdb_ptr &tdb_module_entry
 
-#define PHP_TDB_VERSION "0.2.0-dev"
+#define PHP_TDB_VERSION "1.0.0"
 
 #ifdef PHP_WIN32
 #define PHP_TDB_API __declspec(dllexport)
